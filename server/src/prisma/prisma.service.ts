9a94183f85a3efd2d6b0a9418ccd05019f1fb0b9
boburov <boburovdev@gmail.com>
2026-05-11 00:33:09 +0500

    backend database confugraion ended

---
+++ server/src/prisma/prisma.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client';
+import { PrismaMariaDb } from '@prisma/adapter-mariadb';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    const adapter = new PrismaMariaDb({
+      host: process.env.DB_HOST ?? 'localhost',
+      user: process.env.DB_USER ?? 'root',
+      password: process.env.DB_PASSWORD ?? '',
+      database: process.env.DB_NAME ?? 'your_db',
+    });
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+  }
+}
